8c886650aa65ddb91f7f6251507b63ab40e76600
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-07-17 20:38:01 +0530

    input speech

--- jarvis.py
+++ jarvis.py
@@ -1,1 +1,24 @@
-print("Hello world")
+import speech_recognition as sr
+import os
+import datetime
+from gtts import gTTS
+import wikipedia
+import random
+import warnings
+import pyaudio
+warnings.filterwarnings('ignore')
+
+def recordAudio():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something\n")
+        audio=r.listen(source)
+
+    data=''
+    try:
+        data=r.recognize_google(audio)
+        print("You said: "+data)
+    except:
+        print("Didn't get ya !")
+
+recordAudio()
