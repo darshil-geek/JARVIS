2a617f7f2664778b4612f4b3a3bed05dbede0289
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-07-19 11:33:48 +0530

    added date command

--- jarvis.py
+++ jarvis.py
@@ -6,8 +6,10 @@ import wikipedia
 import random
 import warnings
 import pyaudio
+import calendar
 warnings.filterwarnings('ignore')
 
+#PSS listening to commands
 def recordAudio():
     r= sr.Recognizer()
     with sr.Microphone() as source:
@@ -21,7 +23,13 @@ def recordAudio():
     except:
         print("Didn't get ya !")
 
+    return data
 
+
+
+
+
+#PSS response
 def assistantResponse(text):
     print(text)
     myobj=gTTS(text=text,lang='en',slow=False)
@@ -29,7 +37,71 @@ def assistantResponse(text):
     myobj.save('assistant_response.mp3')
     os.system('afplay assistant_response.mp3')
 
-text="This is a test by my master Darshil"
+    #text="This is a test by my master Darshil"
 
-recordAudio()
-assistantResponse(text)
+
+
+
+
+def wakewords(text):
+    WAKE_WORDS=["hi","hey pss","hi darshil","hai"]
+    text=str(text)
+    text=text.lower()
+
+    for phrase in WAKE_WORDS:
+        if phrase in text:
+            return True
+
+    return False
+
+
+
+
+
+def getDate():
+    now=datetime.datetime.now()
+    my_date=datetime.datetime.today()
+    weekday=calendar.day_name[my_date.weekday()]
+    monthNum= now.month
+    daynum=now.day
+
+    month_names=["January","February","March","April","May","June","July","August","September","October","November","December"]
+    ordinal_numbers=["1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th",
+    "15th","16th","17th","18th","19th","20th","21st","22nd","23rd","24th","25th","26th","27th","28th"
+    ,"29th","30th","31st"]
+
+    return "Today is "+weekday+", "+month_names[monthNum-1]+" the "+ordinal_numbers[daynum-1]+"."
+
+
+
+
+
+#returning a greeting
+def greeting(text):
+    GREETING_INPUTS=["hi","hey","ola","kem chho ?"]
+    GREETING_RESPONSES=["hi","hey there","majhama"]
+
+    for word in text.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES) + "."
+            print("there")
+    return "majhama"
+
+
+
+
+while(True):
+
+    text=recordAudio()
+    response=''
+
+    if(wakewords(text)==True):
+        response=response+greeting(text)
+        
+
+        if "date" in text:
+            get_date=getDate()
+            response=response+" "+get_date
+    
+    assistantResponse(response)
+
